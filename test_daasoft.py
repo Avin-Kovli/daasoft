93f7af1da9e5e1489cd115a8dc114ba4d07b1059
Avin-Kovli <aveen.mohammed@uod.ac>
2025-09-09 10:07:54 +0300

    Delete test_daasoft.py

--- test_daasoft.py
+++
@@ -1,4 +0,0 @@
-from daasoft import normalize_text, Words2Numbers, AliK2Unicode
-s = "هاوڕێکان ــ و خاتوو"
-print("Normalized:", normalize_text(s))
-print("Words2Numbers('هەژدە') ->", Words2Numbers("هەژدە"))
